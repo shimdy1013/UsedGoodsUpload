43bafc7920e4ca849496e21cab43dd974344bb38
shimdy1013 <shimdy1013@naver.com>
2023-06-19 22:10:34 +0900

    first commit

---
+++ UsedGoodsUpload/MainViewController.swift
@@ -0,0 +1,14 @@
+//
+//  MainViewController.swift
+//  UsedGoodsUpload
+//
+//  Created by 심두용 on 2023/06/19.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+class MainViewController: UIViewController {
+    
+}
